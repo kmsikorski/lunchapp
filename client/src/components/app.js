5a0fd4a522da534ac1247baf03323ef1f8ee7f92
Karolina <karolinarogusz@onet.pl>
2020-02-10 23:19:30 +0100

    header added

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './app.css';
+import Header from './header/header.js'
 
 class App extends React.Component {
 
@@ -14,19 +15,7 @@ class App extends React.Component {
   render(){
     return (
       <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header />
       </div>
     );
   }
