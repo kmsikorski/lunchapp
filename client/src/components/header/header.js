673a01397747a7aa90db88374623b678734e9f34
Karolina <karolinarogusz@onet.pl>
2020-02-18 18:21:47 +0100

    add Nav component

--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './header.scss';
+import Nav from './nav/nav.jsx'
 
 function Header(){
     return(
@@ -7,6 +8,7 @@ function Header(){
             <div id="page-name">
                 <h1>LunchApp</h1>
             </div>
+            <Nav/>
         </header>
     )
 }
