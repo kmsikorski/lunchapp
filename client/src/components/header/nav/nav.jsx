fb4272182cee4749a955f30e541e796579cd3de3
Karolina <karolinarogusz@onet.pl>
2020-03-17 23:40:05 +0100

    create hamburger icon

--- client/src/components/header/nav/nav.jsx
+++ client/src/components/header/nav/nav.jsx
@@ -4,16 +4,41 @@ import {
 } from "react-router-dom";
 import "./nav.scss";
 
-function Nav(){
-  return(
-    <nav>
-      <ul>
-        <li><Link to="/restaurants">RESTAURANTS</Link></li>
-        <li><Link to="/users">USERS</Link></li>
-        <li><Link to="/lunches">LUNCHES</Link></li>
-      </ul>
-    </nav>      
-  );
+class Nav extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      isHamburgerClicked: false
+    };
+    this.toogleHamburger = this.toogleHamburger.bind(this);
+  }
+
+  toogleHamburger(){
+    this.setState(state => ({
+      isHamburgerClicked: !this.state.isHamburgerClicked
+    }))
+    console.log(this.state.isHamburgerClicked)
+  }
+
+
+  render(){
+    return(
+      <nav>
+        <div className = {`hamburger ${(this.state.isHamburgerClicked? "cross" : "" )}`}
+          onClick = {this.toogleHamburger}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <ul>
+          <li><Link to="/restaurants">RESTAURANTS</Link></li>
+          <li><Link to="/users">USERS</Link></li>
+          <li><Link to="/lunches">LUNCHES</Link></li>
+        </ul>
+      </nav>      
+    );
+  }
+  
 }
 
 export default Nav;
